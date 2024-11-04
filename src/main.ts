49f7c8e015efd0382bf32ab3d4cca497008112ca
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-11-03 18:13:18 -0800

    Added initial button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 // todo
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const uselessButton = document.createElement("button");
+uselessButton.innerHTML = "Click me";
+uselessButton.addEventListener("click", () => {
+  alert("You clicked the button");
+});
+app.append(uselessButton);
