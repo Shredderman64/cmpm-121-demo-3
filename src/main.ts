8d754b14d6d9c3b04388ce09054694ec91b7ae06
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-11-05 23:49:21 -0800

    Added map and player marker

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,40 @@
 // todo
+import leaflet from "leaflet";
+// import luck from "./luck.ts";
+
+import "leaflet/dist/leaflet.css";
 import "./style.css";
 
+import "./leafletWorkaround.ts";
+
 const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const appName = "Technically (not) NFTs";
+document.title = appName;
+
+const header = document.createElement("h1");
+header.innerHTML = appName;
+app.prepend(header);
+
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+const ZOOM_LEVEL = 19;
+
+const map = leaflet.map(document.getElementById("map")!, {
+  center: OAKES_CLASSROOM,
+  zoom: ZOOM_LEVEL,
+  scrollWheelZoom: false,
+});
+
+leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: ZOOM_LEVEL,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+playerMarker.bindTooltip("Hi");
+playerMarker.addTo(map);
 
 const uselessButton = document.createElement("button");
 uselessButton.innerHTML = "Click me";
